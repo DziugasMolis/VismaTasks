6314d9a310d6752c257d4eaa3f65df2ee658fc7e
DESKTOP-QEO2G0G\Dziugas <molisdziugas97@gmail.com>
2017-10-28 16:32:48 +0300

    Fixed Tests code

--- VismaTasks/UnitTestProject1/Tests.cs
+++ VismaTasks/UnitTestProject1/Tests.cs
@@ -10,90 +10,130 @@ namespace TestsForTasks
         [TestMethod]
         public void Task1Test1()
         {
+            // arrange
             string ransom = "a";
             string magazine = "b";
-
             Tasks foo = new Tasks();
 
-            Assert.IsFalse(foo.CanConstruct(ransom, magazine));
+            // act 
+            bool actual = foo.CanConstruct(ransom, magazine);
+
+            // assert
+            Assert.IsFalse(actual);
         }
         [TestMethod]
         public void Task1Test2()
         {
+            // arrange
             string ransom = "aa";
             string magazine = "ab";
-
             Tasks foo = new Tasks();
 
-            Assert.IsFalse(foo.CanConstruct(ransom, magazine));
+            // act 
+            bool actual = foo.CanConstruct(ransom, magazine);
+
+            // assert
+            Assert.IsFalse(actual);
         }
         [TestMethod]
         public void Task1Test3()
         {
+            // arrange
             string ransom = "aa";
             string magazine = "aab";
-
             Tasks foo = new Tasks();
 
-            Assert.IsTrue(foo.CanConstruct(ransom, magazine));
+            // act 
+            bool actual = foo.CanConstruct(ransom, magazine);
+
+            // assert
+            Assert.IsTrue(actual);
         }
         [TestMethod]
         public void Task1Test4()
         {
+            // arrange
             string ransom = "aaa";
             string magazine = "aababa";
-
             Tasks foo = new Tasks();
 
-            Assert.IsTrue(foo.CanConstruct(ransom, magazine));
+            // act 
+            bool actual = foo.CanConstruct(ransom, magazine);
+
+            // assert
+            Assert.IsTrue(actual);
         }
         [TestMethod]
         public void Task1Test5()
         {
+            // arrange
             string ransom = "aaa";
             string magazine = "bbbbb";
-
             Tasks foo = new Tasks();
 
-            Assert.IsFalse(foo.CanConstruct(ransom, magazine));
+            // act 
+            bool actual = foo.CanConstruct(ransom, magazine);
+
+            // assert
+            Assert.IsFalse(actual);
         }
 
         [TestMethod]
         public void Task2Test1()
         {
+            // arrange 
             int[] nums = { 0, 1, 0, 3, 12 };
             int[] answer = { 1, 3, 12, 0, 0 };
             Tasks foo = new Tasks();
 
-            CollectionAssert.AreEqual(answer, foo.Zeros(nums));
+            // act 
+            int[] actual = foo.Zeros(nums);
+
+            // assert
+            CollectionAssert.AreEqual(answer, actual);
 
         }
         [TestMethod]
         public void Task2Test2()
         {
+            // arrange
             int[] nums = { 0, 0, 0, 0, 1 };
             int[] answer = { 1, 0, 0, 0, 0 };
             Tasks foo = new Tasks();
 
-            CollectionAssert.AreEqual(answer, foo.Zeros(nums));
+            // act 
+            int[] actual = foo.Zeros(nums);
+
+            // assert
+            CollectionAssert.AreEqual(answer, actual);
         }
         [TestMethod]
         public void Task2Test3()
         {
+            // arrange
             int[] nums = { 1, 2, 3, 4, 5 };
             int[] answer = { 1, 2, 3, 4, 5 };
             Tasks foo = new Tasks();
 
-            CollectionAssert.AreEqual(answer, foo.Zeros(nums));
+            // act 
+            int[] actual = foo.Zeros(nums);
+
+            // assert
+            CollectionAssert.AreEqual(answer, actual);
         }
         [TestMethod]
         public void Task2Test4()
         {
+            // arrange
             int[] nums = { 1, 2, 0, 0, 3, 0, 4 };
             int[] answer = { 1, 2, 3, 4, 0, 0, 0 };
             Tasks foo = new Tasks();
 
-            CollectionAssert.AreEqual(answer, foo.Zeros(nums));
+            // act 
+            int[] actual = foo.Zeros(nums);
+
+            // assert
+            CollectionAssert.AreEqual(answer, actual);
         }
 
     }
