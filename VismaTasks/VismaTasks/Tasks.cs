5393d028bbc7a2aa04c223961ff3f9a3137282b8
DESKTOP-QEO2G0G\Dziugas <molisdziugas97@gmail.com>
2017-10-28 16:27:28 +0300

    Fixed naming and spacing

--- VismaTasks/VismaTasks/Tasks.cs
+++ VismaTasks/VismaTasks/Tasks.cs
@@ -21,13 +21,11 @@ namespace VismaTasks
 
             int k = 0;
            
-
             if (ransomCount > magazineCount)
                 return false;
 
             for (int i = 0; i < ransomCount; i++)
             {
-
                 for (int j = i; j < magazineCount; j++)
                 {
                     if (ransomArray[i].Equals(magazineArray[j]))
@@ -37,28 +35,31 @@ namespace VismaTasks
                     }
                 }
             }
+
             if (k == ransomCount)
+            {
                 return true;
-
+            }
+        
             return false;
-
         }
         public int[] Zeros(int[] nums)
         {
-
             int positiveNumber = 0;
+
             for (int i = 0; i < nums.Length; i++)
             {
                 if (nums[i] != 0)
                 {
-
 
                     if (nums[positiveNumber] != nums[i])
                         nums[positiveNumber] = nums[i];
                     positiveNumber++;
                 }
             }
+
             int zerosCount = nums.Length - positiveNumber;
+
             if (zerosCount != 0)
             {
                 for (int i = 0; i < zerosCount; i++)
@@ -66,6 +67,7 @@ namespace VismaTasks
                     nums[nums.Length - i - 1] = 0;
                 }
             }
+
             return nums;
         }
     }
